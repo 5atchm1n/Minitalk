28c7bf6c4cc09a2b9c4a8b85db360daf7d369692
Satcheen SHAKYA <sshakya@student.42.fr>
2021-06-29 16:31:02 +0200

    :checkered_flag: clean
    *
    
    *

--- objs/mn_client.d
+++
@@ -1,5 +0,0 @@
-objs/mn_client.o: src/mn_client.c inc/minitalk.h inc/libft/inc/libft.h
-
-inc/minitalk.h:
-
-inc/libft/inc/libft.h:
--- objs/mn_server.d
+++
@@ -1,5 +0,0 @@
-objs/mn_server.o: src/mn_server.c inc/minitalk.h inc/libft/inc/libft.h
-
-inc/minitalk.h:
-
-inc/libft/inc/libft.h:
